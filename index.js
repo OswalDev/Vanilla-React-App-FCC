a6156f0fc00b1697afeee4936698db65b0c9c18b
OswalDev <HardCodedDev@protonmail.com>
2022-04-26 23:56:35 -0400

    .append instead of ReactDOM

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
+import React from "react";
+
+const page = (
+
+    <div>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+        </ul>
+        <h1 className="title">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+);
+
+ReactDOM.render(
+ page,
+ document.getElementById("root").append(JSON.stringify(page))
+ ); 
